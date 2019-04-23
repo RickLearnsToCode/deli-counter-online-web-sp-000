99975ecfdc51816358494c6b77a517fec4d017d1
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-23 13:21:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+katz_deli= ["Rick", "John", "Marc"]
+
+def line(current_line)
+  if current_line.size == 0
+    puts "The line is currently empty."
+  else
+    line_list = []
+    line_list << current_line.each_with_index {|name, index| "#{index+1} #{name}"}
+    puts line_list
+  end
+
+def take_a_number(current_line, name)
+  current_line << name
+  puts "Welcome, #{name}.  You are number #{current_line.size} in line."
+end
+
+line(katz_deli)
+
+
+  
